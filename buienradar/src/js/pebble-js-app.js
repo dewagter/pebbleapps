c585a34fd032b7558774b2bf5c8627a506056de0
Christophe De Wagter <dewagter@gmail.com>
2013-11-18 11:10:02 +0100

    BuienRadar

---
+++ buienradar/src/js/pebble-js-app.js
@@ -0,0 +1,23 @@
+Pebble.addEventListener("ready",
+    function(e) {
+        console.log("Buienradar: Ready Event.");
+        var req = new XMLHttpRequest();
+        req.open('GET', "http://gps.buienradar.nl/getrr.php?lat=52.3982969&lon=4.6431016", true);
+        req.onload = function(e) {
+          if (req.readyState == 4) {
+            if(req.status == 200 /* 200 - HTTP OK */) {
+              console.log(req.responseText);
+              response = req.responseText;
+              Pebble.showSimpleNotificationOnPebble("BuienRadar", response);
+            } else {
+              console.log("Request returned error code " + req.status.toString());
+            }
+          }
+          else
+          {
+            console.log("Request not state 4 " + req.readyState.toString());
+          }
+        }
+        req.send(null);
+    }
+);
