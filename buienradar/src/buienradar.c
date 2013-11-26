eec634aebb4ab5d3ee60360cc48364631a694cad
Christophe De Wagter <dewagter@gmail.com>
2013-11-26 10:27:58 +0100

    BuienRadar With Status

--- buienradar/src/buienradar.c
+++ buienradar/src/buienradar.c
@@ -1,18 +1,80 @@
 #include <pebble.h>
 
-static Window *window;
-static TextLayer *text_layer;
+static TextLayer *status_layer;
+
+
+enum {
+  BUI_KEY_STATUS = 0x0,
+  BUI_KEY_FETCH = 0x1,
+};
+
+///////////////////////////////////////////////////////////
+// SEND
+
+static void fetch_buienradar() {
+  Tuplet station_tuple = TupletCString(BUI_KEY_FETCH, "here");
+
+  DictionaryIterator *iter;
+  app_message_outbox_begin(&iter);
+
+  if (iter == NULL) {
+    return;
+  }
+
+  dict_write_tuplet(iter, &station_tuple);
+  dict_write_end(iter);
+
+  app_message_outbox_send();
+  text_layer_set_text(status_layer, "App sent request.");
+}
+
+///////////////////////////////////////////////////////////
+// RECEIVE
+
+static void in_received_handler(DictionaryIterator *iter, void *context) {
+  Tuple *status_tuple = dict_find(iter, BUI_KEY_STATUS);
+
+  if (status_tuple) {
+    text_layer_set_text(status_layer, status_tuple->value->cstring);
+  }
+}
+
+static void in_dropped_handler(AppMessageResult reason, void *context) {
+  text_layer_set_text(status_layer, "Msg dropped...");
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "App Message Dropped!");
+}
+
+static void out_failed_handler(DictionaryIterator *failed, AppMessageResult reason, void *context) {
+  text_layer_set_text(status_layer, "Msg failed to send.");
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "App Message Failed to Send!");
+}
+
+static void app_message_init(void) {
+  // Register message handlers
+  app_message_register_inbox_received(in_received_handler);
+  app_message_register_inbox_dropped(in_dropped_handler);
+  app_message_register_outbox_failed(out_failed_handler);
+  // Init buffers
+  app_message_open(64, 64);
+}
+
+
+
+
+///////////////////////////////////////////////////////////
+// BUTTONS
 
 static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(text_layer, "Select");
+  text_layer_set_text(status_layer, "Request BuienRadar");
+  fetch_buienradar();
 }
 
 static void up_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(text_layer, "Up");
+  //text_layer_set_text(status_layer, "Up");
 }
 
 static void down_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(text_layer, "Down");
+  //text_layer_set_text(status_layer, "Down");
 }
 
 static void click_config_provider(void *context) {
@@ -21,22 +83,40 @@ static void click_config_provider(void *context) {
   window_single_click_subscribe(BUTTON_ID_DOWN, down_click_handler);
 }
 
+///////////////////////////////////////////////////////////
+// WINDOW (UN)LOAD
+
+static Window *window;
+static TextLayer* s1_layer;
+
 static void window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
 
-  text_layer = text_layer_create((GRect) { .origin = { 0, 72 }, .size = { bounds.size.w, 20 } });
-  text_layer_set_text(text_layer, "BuienRadar");
-  text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
-  layer_add_child(window_layer, text_layer_get_layer(text_layer));
+  s1_layer = text_layer_create((GRect) { .origin = { 0, 62 }, .size = { bounds.size.w, 20 } });
+  text_layer_set_text(s1_layer, "BuienRadar>");
+  text_layer_set_text_alignment(s1_layer, GTextAlignmentRight);
+  text_layer_set_font(s1_layer, fonts_get_system_font(FONT_KEY_GOTHIC_14_BOLD));
+  layer_add_child(window_layer, text_layer_get_layer(s1_layer));
+
+  status_layer = text_layer_create((GRect) { .origin = { 0, 120 }, .size = { bounds.size.w, 20 } });
+  text_layer_set_text(status_layer, "App Started");
+  text_layer_set_font(status_layer, fonts_get_system_font(FONT_KEY_GOTHIC_14));
+  text_layer_set_text_alignment(status_layer, GTextAlignmentCenter);
+  layer_add_child(window_layer, text_layer_get_layer(status_layer));
 }
 
 static void window_unload(Window *window) {
-  text_layer_destroy(text_layer);
+  text_layer_destroy(status_layer);
+  text_layer_destroy(s1_layer);
 }
+
+///////////////////////////////////////////////////////////
+// APP (DE)INIT
 
 static void init(void) {
   window = window_create();
+  app_message_init();
   window_set_click_config_provider(window, click_config_provider);
   window_set_window_handlers(window, (WindowHandlers) {
     .load = window_load,
@@ -49,6 +129,9 @@ static void init(void) {
 static void deinit(void) {
   window_destroy(window);
 }
+
+///////////////////////////////////////////////////////////
+// MAIN
 
 int main(void) {
   init();
--- buienradar/src/js/pebble-js-app.js
+++ buienradar/src/js/pebble-js-app.js
@@ -1,23 +1,53 @@
+function fetchBuienRadar() {
+    var req = new XMLHttpRequest();
+    req.open('GET', "http://gps.buienradar.nl/getrr.php?lat=52.3982969&lon=4.6431016", true);
+    req.onload = function(e) {
+      if (req.readyState == 4) {
+        if(req.status == 200 /* 200 - HTTP OK */) {
+          console.log(req.responseText);
+          response = req.responseText;
+          // if (response.length > 40)
+
+          Pebble.showSimpleNotificationOnPebble("BuienRadar", "Haarlem\n" + response);
+        } else {
+          console.log("Request returned error code " + req.status.toString());
+          Pebble.sendAppMessage({"status": "JS Error Response"});
+        }
+      }
+      else
+      {
+        console.log("Request not state 4 " + req.readyState.toString());
+        Pebble.sendAppMessage({"status": "JS Error State"});
+      }
+    };
+    req.onerror = function (e) {
+       Pebble.sendAppMessage({"status": "No internet"});
+    };
+    req.send(null);
+    Pebble.sendAppMessage({"status": "JS waits for http"});
+}
+
+
+
 Pebble.addEventListener("ready",
     function(e) {
-        console.log("Buienradar: Ready Event.");
-        var req = new XMLHttpRequest();
-        req.open('GET', "http://gps.buienradar.nl/getrr.php?lat=52.3982969&lon=4.6431016", true);
-        req.onload = function(e) {
-          if (req.readyState == 4) {
-            if(req.status == 200 /* 200 - HTTP OK */) {
-              console.log(req.responseText);
-              response = req.responseText;
-              Pebble.showSimpleNotificationOnPebble("BuienRadar", "Haarlem\n" + response);
-            } else {
-              console.log("Request returned error code " + req.status.toString());
-            }
-          }
-          else
-          {
-            console.log("Request not state 4 " + req.readyState.toString());
-          }
-        }
-        req.send(null);
+        console.log("Buienradar: ReadyEvent=" + e.ready + "Type=" + e.type);
+        Pebble.sendAppMessage({"status": "JS ready"});
     }
 );
+
+// Set callback for appmessage events
+Pebble.addEventListener("appmessage",
+    function(e) {
+      console.log("message");
+      if (e.payload.fetch) {
+        //localStorage.setItem("symbol", symbol);
+        console.log("fetch");
+        fetchBuienRadar();
+      }
+    }
+);
+
+
+
+
